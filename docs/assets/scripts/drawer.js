b8ec0473afdfce3a30c4fbd1604facc66dd66d46
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-09-09 19:39:04 +0200

    :heavy_plus_sign: Handle drawer opening on mobiles

---
+++ docs/assets/scripts/drawer.js
@@ -0,0 +1,22 @@
+var toggles = document.querySelectorAll( "[data-toggle]" );
+
+console.log( toggles );
+
+toggles.forEach( function ( toggle ) {
+    toggle.addEventListener( "click", function ( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var drawerId      = toggle.dataset.toggle;
+        var drawerElem    = document.getElementById( drawerId );
+        var containerElem = document.getElementById( "container" );
+
+        if ( drawerElem ) {
+            drawerElem.classList.toggle( "is-open" );
+            containerElem.classList.toggle( "is-drawer-open" );
+            document.body.classList.toggle( "is-drawer-open" );
+        } else {
+            console.error( "No drawer with id " + drawerId + " found" );
+        }
+    }, false );
+} );
